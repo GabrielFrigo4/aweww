d7c69a28fde47abe50ede1efd6a2b24c49778852
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2025-04-06 13:46:57 -0300

    Init AWEWW

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# aweww
-Awesome EWW extension base on EWW with wonderful features!
+# What is Aweww?
+ Awesome EWW extension base on EWW with wonderful features!
+
+# Improve EWW / SHR with SHRFACE
+ The [shrface](https://github.com/chenyanming/shrface) extends `shr` / `eww` with org features and analysis capability.
+
+# Installing with Quelpa
+ If you prefer to use a package manager, you can use [quelpa-use-package](https://github.com/quelpa/quelpa-use-package).
+
+ ```elisp
+ ;; Install Aweww
+ (use-package aweww
+   :quelpa (aweww :fetcher github :repo "GabrielFrigo4/aweww"))
+ ```
---
+++ aweww.el
@@ -0,0 +1,56 @@
+;;; aweww.el --- Awesome EWW
+
+;; ################
+;; # EWW
+;; ################
+
+;; Cleanup New Lines
+(defun aweww-cleanup-newlines ()
+  "Remove Excessive Blank Lines in AWEWW Buffers."
+  (let ((inhibit-read-only t))
+    (save-excursion
+      (goto-char (point-min))
+      (while (re-search-forward "\n\\{3,\\}" nil t)
+        (replace-match "\n\n")))))
+(add-hook 'aweww-after-render-hook #'aweww-cleanup-newlines)
+
+;; ################
+;; # Renders
+;; ################
+
+;; Create *EWW After Section Header*
+(defvar aweww-after-section-header nil
+  "AWEWW After Section Header")
+
+;; Restart *EWW After Section Header*
+(add-hook 'aweww-after-render-hook
+          (lambda ()
+            (setq aweww-after-section-header nil)))
+
+;; Create <b> Render for EWW / SHR
+(defun aweww-render-b (dom)
+  "Custom <b> rendering for AWEWW: Headers and Body"
+  (let* ((class (dom-attr dom 'class))
+         (text (dom-texts dom)))
+    (cond
+     ;; Section Header: Insert Spacing and New Line
+     ((and class (string= class "section"))
+      (shr-insert "\n\n")
+      (shr-generic dom)
+      (shr-insert "\n\n")
+      (setq aweww-after-section-header t))
+     ;; Bold Text: Indent if at Beginning of Line
+     ((and text
+           (or (string-match-p "^[a-zA-Z0-9_]+$" text)
+               (string-match-p "^#" text)))
+      (when (and (save-excursion (bolp))
+                 (eq aweww-after-section-header t))
+        (shr-insert "     ")
+        (setq aweww-after-section-header nil))
+      (shr-insert (propertize text 'face 'bold)))
+     ;; Fallback
+     (t
+      (shr-generic dom)))))
+(add-to-list 'shr-external-rendering-functions '(b . aweww-render-b))
+
+;;; aweww.el ends here

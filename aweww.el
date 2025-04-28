1e2cabd8d529bd2e19733111113bfdaaa22cb9d1
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2025-04-28 01:26:20 -0300

    Update aweww-shr-tag-pre-highlight

--- aweww.el
+++ aweww.el
@@ -1,8 +1,9 @@
 ;;; aweww.el --- Awesome EWW
 
 ;; TODO
+;; [ ] Improve HTML Rendering Indentation
 ;; [ ] Improve Dark / Light Themes
-;; [X] Improve HTML Rendering Indentation
+;; [X] Improve Render New Lines 
 ;; [X] Improve Code Highlight Scope
 
 ;; ################
@@ -29,25 +30,6 @@
 ;; Import SHRFACE
 (require 'shrface)
 
-;; Cleanup New Lines
-(defun aweww-cleanup-newlines ()
-  "Remove Excessive Blank Lines in AWEWW Buffers."
-  (let ((inhibit-read-only t))
-    (save-excursion
-      (goto-char (point-min))
-      (while (re-search-forward "\n\\{3,\\}" nil t)
-        (replace-match "\n\n")))))
-
-;; Cleanup New Lines Deferred
-(defun aweww-cleanup-newlines-deferred ()
-  "Defer cleanup until after EWW finishes rendering."
-  (run-at-time 0 nil #'aweww-cleanup-newlines))
-
-;; Setup Shrface in EWW
-(defun shrface-eww-setup ()
-  (unless shrface-toggle-bullets
-    (shrface-regexp)))
-
 ;; AWEWW General Rendering
 (defvar aweww-general-rendering-functions
   (append '((title . eww-tag-title)
@@ -66,15 +48,24 @@
 ;; Enable Shrface in AWEWW
 (setq-default shr-external-rendering-functions aweww-general-rendering-functions)
 
-;; AWEWW Render Advice
-;;(defun aweww-render-advice (orig-fun &rest args)
-;;  (let ((shrface-org nil)
-;;        (shr-bullet (concat (char-to-string shrface-item-bullet) " "))
-;;        (shr-width 128)
-;;        (shr-indentation 0)
-;;        (shr-external-rendering-functions aweww-general-rendering-functions)
-;;        (shr-use-fonts nil))
-;;    (apply orig-fun args)))
+;; Cleanup New Lines
+(defun aweww-cleanup-newlines ()
+  "Remove Excessive Blank Lines in AWEWW Buffers."
+  (let ((inhibit-read-only t))
+    (save-excursion
+      (goto-char (point-min))
+      (while (re-search-forward "\n\\{3,\\}" nil t)
+        (replace-match "\n\n")))))
+
+;; Cleanup New Lines Deferred
+(defun aweww-cleanup-newlines-deferred ()
+  "Defer cleanup until after EWW finishes rendering."
+  (run-at-time 0 nil #'aweww-cleanup-newlines))
+
+;; Setup Shrface in EWW
+(defun shrface-eww-setup ()
+  (unless shrface-toggle-bullets
+    (shrface-regexp)))
 
 ;; Updae EWW Render
 (add-hook 'eww-after-render-hook #'shrface-eww-setup)
